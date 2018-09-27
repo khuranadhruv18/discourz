5750f9f63bc40b018a34f7b1d4ade5dafbeec43a
Prakrit Saetang <junjang_prakit@hotmail.com>
2018-09-26 23:11:18 -0400

    update UI

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
+
+$(document).ready(function(){
+	resizeContent();
+
+    $(window).resize(function() {
+        resizeContent();
+    });
+});
+
+	
+function resizeContent() {
+	var width = $(window).width();
+	var sidebarWidth = $('#mySidenav').width();
+	$('.content').css('width', (width - sidebarWidth) + 'px');
+}
+
+function toggleNav2() {
+	var width = $(window).width();
+	var x = $('#mySidenav');
+	var elements = $('.sidebar-text');
+	var content = $('.content');
+
+
+	if (x.width() !== 160) {
+		x.css('width', '160px');
+		elements.css('color', '#818181');
+		content.css('left', '160px');
+		content.css('width', (width - 160) + 'px');
+	}
+	else {
+		x.css('width', '45px');
+		elements.css('color', '#111');
+		content.css('left', '45px');
+		content.css('width', (width - 45) + 'px');
+	}
+}
+
+
 function toggleNav() {
 	var x = document.getElementById("mySidenav").style.width;
 	var elements = document.querySelectorAll('.sidebar-text');
@@ -8,7 +46,7 @@ function toggleNav() {
 		for(var i=0; i<elements.length; i++) {
     		elements[i].style.color = "#818181";
 		}
-		content[0].style.left = "160px";
+		content[0].style.left = "160px"; 
 	}
 	else {
 		document.getElementById("mySidenav").style.width = "45px";
@@ -16,7 +54,7 @@ function toggleNav() {
     		elements[i].style.color = "#111";
 		}
 		content[0].style.left = "45px";
+		
 	}
-         
+    
 }
-
